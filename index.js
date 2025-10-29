bca363a9d73a7b306e8ed2e5711139c23368a67f
bashar abu hannoud <bashar.abuhannoud@opensooq.com>
2025-10-29 07:07:22 +0300

    test ci/cd

--- index.js
+++ index.js
@@ -2,4 +2,12 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => res.send('Hello CI/CD!'));
+
+app.post('/', (req, res) => {
+    const body = req.body;
+    console.log(body);
+
+    return res.status(200).send(body);
+});
+
 app.listen(3000, () => console.log('Server running on port 3000'));
